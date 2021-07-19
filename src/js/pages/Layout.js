7d02554428c04e6140fb57b2c49dbe79d8b6a176
kn-sato <kn_sato@innovation.co.jp>
2021-07-19 11:28:00 +0900

    add: 各ページ追加、Router記載、リンク追加

---
+++ src/js/pages/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="/archives" class="btn btn-danger">archives</Link>
+        <Link to="/settings" class="btn btn-success">settings</Link>
+      </div>
+    );
+  }
+}
