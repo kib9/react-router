99df5d939a6ff9dd8c4012e9649bd36867f5b010
kn-sato <kn_sato@innovation.co.jp>
2021-07-20 10:39:12 +0900

    edit: cmponent 分割

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link, withRouter, NavLink } from "react-router-dom";
 
+import Nav from "../components/layout/Nav";
+import Footer from "../components/layout/Footer";
+
 class Layout extends React.Component {
   navigate = () => {
     console.log(this.props.history);
@@ -9,15 +12,28 @@ class Layout extends React.Component {
   }
 
   render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+
     return (
       <div>
-        <h1>KillerNews.net</h1>
-        {this.props.children}
-        <Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other articles)</Link>
-        <Link to="/archives?date=today&filter=hot" class="btn btn-danger">archives</Link>
-        <NavLink to="/settings/main" class="btn btn-success" activeClassName="btn btn-danger">settings</NavLink>
-        <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
-        <button class="btn btn-info" onClick={this.navigate}>featured</button>
+        <Nav location={location} />
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>KillerNews.net</h1>
+              <Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other articles)</Link>
+              <Link to="/archives?date=today&filter=hot" class="btn btn-danger">archives</Link>
+              <NavLink to="/settings/main" class="btn btn-success" activeClassName="btn btn-danger">settings</NavLink>
+              <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
+              <button class="btn btn-info" onClick={this.navigate}>featured</button>
+              {this.props.children}
+            </div>
+          </div>
+          <Footer/>
+        </div>
       </div>
     );
   }
